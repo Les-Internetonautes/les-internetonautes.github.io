a0f160b1c442e99aedd56322b4fa96ac31b85b7f
Yannick Debrée <debree.yannick13@gmail.com>
2021-09-02 22:32:46 +0200

    feat: brand section in progress

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let isMobileMenuOpened = false;
+const mobileMenu = document.querySelector("#mobile-menu");
+
+function toggleMobileMenu() {
+  isMobileMenuOpened = !isMobileMenuOpened;
+
+  if (isMobileMenuOpened) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+}
+
+function initMobileMenuDisplayChangesDetection() {
+  const hamburger = document.querySelector("#hamburger");
+  const mobileMenuCross = document.querySelector("#mobile-menu-cross");
+
+  hamburger.addEventListener("click", toggleMobileMenu);
+  mobileMenuCross.addEventListener("click", toggleMobileMenu);
+}
+
+function main() {
+  initMobileMenuDisplayChangesDetection();
+}
+
+window.addEventListener("load", main);
